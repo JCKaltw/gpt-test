05f5dfaab0d970ee78978700f327429ee70a5bfb
JCKalt <ckaltwasser@reminingcorp.com>
2025-02-22 12:12:21 -0500

    added more tests

---
+++ bin/test-validate-dml-ast-small.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+. ./source-venv.sh
+
+echo "python src/validate-json.py --verbose \ "
+echo "    --json-schema-file schemas/dml-ast-schema.json \ "
+echo "    data/dml-ast-small.json "
+
+python src/validate-json.py --verbose \
+    --json-schema-file schemas/dml-ast-schema.json \
+    data/dml-ast-small.json
